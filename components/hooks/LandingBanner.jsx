4e238adde5ab4744aab0c8855c8d26fdb322184b
WEST CODE <devkingdom7@gmail.com>
2021-10-25 18:22:31 +0100

    fifth commit

--- components/hooks/LandingBanner.jsx
+++ components/hooks/LandingBanner.jsx
@@ -1,7 +1,7 @@
 export default function LandingBanner() {
  return (
   <>
-   <main className="max-w-4xl mx-auto my-5 border-8 border-white rounded-lg  flex justify-center">
+   <main className="max-w-6xl mx-auto my-5 border-8 border-white rounded-lg  flex justify-center">
     <img
      src="/image/singlebanner2.png"
      className="w-full object-contain-scale"
